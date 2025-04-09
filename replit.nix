7aa2c9a8ab7c16b9423cbfaa8c92784efbddfe13
Rupsaban04 <160893114+Rupsaban04@users.noreply.github.com>
2025-04-10 00:33:33 +0530

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,22 @@
+{pkgs}: {
+  deps = [
+    pkgs.which
+    pkgs.libpng
+    pkgs.libjpeg_turbo
+    pkgs.zlib
+    pkgs.tk
+    pkgs.tcl
+    pkgs.openjpeg
+    pkgs.libxcrypt
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.freetype
+    pkgs.libGLU
+    pkgs.libGL
+    pkgs.postgresql
+    pkgs.glibcLocales
+  ];
+}
